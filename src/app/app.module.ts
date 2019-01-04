81d4712a9b6bb69fe5d3c356674fa99c79e00779
leonardo15043 <lahernandez15043@gmail.com>
2019-01-03 21:16:00 -0500

    Pipes de texto

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule , LOCALE_ID} from '@angular/core';
+import { AppComponent } from './app.component';
+
+
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEs);
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es'}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
